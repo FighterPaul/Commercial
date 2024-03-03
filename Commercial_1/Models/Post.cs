5ebf95e26587fc3813667f3f1c38aaf6b62f94e9
65010902_Racha_Tungtrakool <65010902@kmitl.ac.th>
2024-03-03 13:55:43 +0700

    Update Post.cs

--- Commercial_1/Models/Post.cs
+++ Commercial_1/Models/Post.cs
@@ -2,11 +2,17 @@
 {
     public class Post
     {
+
+
+
         public int PostId { get; set; }
 
         public string Title { get; set; }
 
         public User Creator { get; set; }
+
+        public string Description { get; set; }
+
 
         public List<User_Post_Member> Member_List { get; set; }
 
